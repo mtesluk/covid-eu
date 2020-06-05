f8a0f89ce38f97db5693fb4e5922d5fb571c2bfd
mtesluk <mateusz@tesluk.pl>
2020-06-05 18:41:32 +0200

    Update color range

--- src/shared/config.tsx
+++ src/shared/config.tsx
@@ -9,7 +9,7 @@ export const rangesConfigCases: RangeConfig = {
     {min: 1001, max: 10000, color: '#fdcd8b'},
     {min: 10001, max: 50000, color: '#b55440'},
     {min: 50001, max: 100000, color: '#b53828'},
-    {min: 100001, max: 420001, color: mostCommonColor},
+    {min: 100001, max: 920000, color: mostCommonColor},
   ],
   default: '#000',
 };
